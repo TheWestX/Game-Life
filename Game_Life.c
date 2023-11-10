1cb0c9705b97ea5f8dcd69033d66fa8cc99425f8
TheWestX <97839604+TheWestX@users.noreply.github.com>
2023-11-10 22:58:36 +0300

    Add files via upload

--- Game_Life.c
+++ Game_Life.c
@@ -7,25 +7,25 @@
 #define HEIGHT 40
 #define GENERATIONS 100
 
-int grid[WIDTH][HEIGHT];
-int newGrid[WIDTH][HEIGHT];
+int grid[WIDTH][HEIGHT];  // Declare an array to store the current state of cells
+int newGrid[WIDTH][HEIGHT];  // Declare an array to store the next state of cells
 
 void clearScreen() {
-    COORD topLeft = {0, 0};
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO screen;
+    COORD topLeft = {0, 0};  // Create a COORD structure with coordinates for the top-left corner
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);  // Get the console handle
+    CONSOLE_SCREEN_BUFFER_INFO screen;  // Create a structure for console information
     DWORD written;
 
-    GetConsoleScreenBufferInfo(console, &screen);
-    FillConsoleOutputCharacterA(console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
-    FillConsoleOutputAttribute(console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
-    SetConsoleCursorPosition(console, topLeft);
+    GetConsoleScreenBufferInfo(console, &screen);  // Get information about the console
+    FillConsoleOutputCharacterA(console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);  // Clear the screen by filling it with spaces
+    FillConsoleOutputAttribute(console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, screen.dwSize.X * screen.dwSize.Y, topLeft, &written);  // Set the color for clearing
+    SetConsoleCursorPosition(console, topLeft);  // Set the cursor position to the top-left corner
 }
 
 void initializeGrid() {
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
-            grid[i][j] = rand() % 2;
+            grid[i][j] = rand() % 2;  // Initialize the grid with random values of 0 or 1
         }
     }
 }
@@ -36,12 +36,12 @@ int countNeighbors(int x, int y) {
 
     for (dx = -1; dx <= 1; dx++) {
         for (dy = -1; dy <= 1; dy++) {
-            if (dx == 0 && dy == 0) continue;
+            if (dx == 0 && dy == 0) continue;  // Skip the current cell
             int nx = x + dx;
             int ny = y + dy;
 
             if (nx >= 0 && nx < WIDTH && ny >= 0 && ny < HEIGHT) {
-                count += grid[nx][ny];
+                count += grid[nx][ny];  // Count the number of live neighbors for a given cell
             }
         }
     }
@@ -52,10 +52,10 @@ int countNeighbors(int x, int y) {
 void updateGrid() {
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
-            int neighbors = countNeighbors(i, j);
+            int neighbors = countNeighbors(i, j);  // Count neighbors for cell (i, j)
 
             if (grid[i][j] == 1) {
-                newGrid[i][j] = (neighbors == 2 || neighbors == 3) ? 1 : 0;
+                newGrid[i][j] = (neighbors == 2 || neighbors == 3) ? 1 : 0;  // Update the cell's state based on the number of neighbors
             } else {
                 newGrid[i][j] = (neighbors == 3) ? 1 : 0;
             }
@@ -64,7 +64,7 @@ void updateGrid() {
 
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
-            grid[i][j] = newGrid[i][j];
+            grid[i][j] = newGrid[i][j];  // Update the current grid based on the new grid
         }
     }
 }
@@ -74,9 +74,9 @@ void drawGrid() {
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
             if (grid[i][j] == 1) {
-                printf("^");
+                printf("^");  // Print a symbol for a live cell
             } else {
-                printf(" ");
+                printf(" ");  // Print a space for an empty cell
             }
         }
         printf("\n");
@@ -99,7 +99,7 @@ int main() {
     for (int generation = 0; generation < GENERATIONS; generation++) {
         drawGrid();
         updateGrid();
-        usleep(500000); // Пауза 500 миллисекунд (0.5 секунды)
+        usleep(500000); // Pause for 500 milliseconds (0.5 seconds)
     }
 
     return 0;
